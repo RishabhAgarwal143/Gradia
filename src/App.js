45134ada431df82736894f8783221ce65d93c785
RishabhAgarwal143 <rish2001.ra@gmail.com>
2023-12-20 23:07:58 -0600

    Authentication added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import '@aws-amplify/ui-react/styles.css'
+import {withAuthenticator,AmplifySignOut} from '@aws-amplify/ui-react';
+import { signOut } from 'aws-amplify/auth';
 
+async function handleSignOut() {
+  try {
+    await signOut();
+  } catch (error) {
+    console.log('error signing out: ', error);
+  }
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <h2>My app</h2>
+          <button onClick={handleSignOut}>Sign out</button>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
