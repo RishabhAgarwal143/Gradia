e477dda5b2879016e4a4ea5fb9df4fe268d2cf2f
mahes <maheshmadhavan2002@gmail.com>
2024-04-23 19:40:09 -0400

    [Topic]: Whisper for audio transcription
    [Body]: Added whisper for audio transcription
    
    [Checklist]:
    
    - [X] Feature
    - [ ] Modification
    - [ ] Bug Fix
    
    [Issue #]: Reference to related issues or tasks
    
    [Method]:

---
+++ Flask_server/audio_listener.py
@@ -0,0 +1,25 @@
+from openai import OpenAI
+
+def initialize():
+    client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+    return client
+
+def transcribe(audio_path):
+    client = initialize()
+    audio_file = open(audio_path, "rb")
+    transcription = client.audio.transcriptions.create(
+        model = "whisper-1",
+        file=audio_file,
+        response_format="json",
+        prompt="Transcibe the audio as input to a smart calendar chatbot",
+        language="en",
+        temperature=0.4
+        )
+    
+    audio_json = transcription.model_dump()
+    plain_text = audio_json.get('text')
+    print(transcription)
+    return plain_text
+
+if __name__ == "__main__":
+    transcribe("output.wav")
