9f4b9c5965bbcba6f51d10455034382df154b487
mahes <maheshmadhavan2002@gmail.com>
2024-05-05 13:18:52 -0400

    [Topic]: Amplify update
    [Body]: Detailed explanation (if necessary)
    
    [Checklist]:
    
    - [ ] Feature
    - [ ] Modification
    - [ ] Bug Fix
    
    [Issue #]: Reference to related issues or tasks
    
    [Method]:

--- src/Components/support_func.js
+++ src/Components/support_func.js
@@ -13,7 +13,8 @@ export var access_Token;
 var CurrentUsersEmail;
 
 // export const backend_Server_ip = "3.21.98.42";
-export const backend_Server_ip = "https://gradia.onrender.com";
+// export const backend_Server_ip = "https://gradia.onrender.com";
+export const backend_Server_ip = "http://142.93.75.16:5000";
 
 export async function currentAuthenticatedUser() {
   try {
